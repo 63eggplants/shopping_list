fe4363a004b738f3248ca7867bca0130417850a5
이하영 <2hayoung@ihayeong-ui-MacBook-Air.local>
2021-07-24 16:27:07 +0900

    JavaScript: Add item

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
 const appLists = document.querySelector(".app__lists");
-const addInput = document.querySelector(".app__addInput");
-const addBtn = document.querySelector(".app__addBtn");
 const deleteBtn = document.querySelector(".list__deleteBtn");
+
 const form = document.querySelector(".app__form");
+const addInput = document.querySelector(".form__addInput");
+const addBtn = document.querySelector(".form__addBtn");
+
+// Add item
+function addList(event) {
+  event.preventDefault();
+  const newItem = addInput.value;
+  addInput.value = "";
+  const li = document.createElement("li");
+  li.setAttribute("class", "list");
+  li.innerHTML = `
+  <span class="list__item">${newItem}</span>
+  <button class="list__deleteBtn">
+    <i class="fas fa-trash-alt"></i>
+  </button>
+  `;
+  appLists.appendChild(li);
+}
+form.addEventListener("submit", addList);
