9c607eaed9c3ffba876b294ef2db18c7a6efcec6
이하영 <2hayoung@ihayeong-ui-MacBook-Air.local>
2021-07-24 16:11:47 +0900

    HTML: Add a form tag

---
+++ main.js
@@ -0,0 +1,5 @@
+const appLists = document.querySelector(".app__lists");
+const addInput = document.querySelector(".app__addInput");
+const addBtn = document.querySelector(".app__addBtn");
+const deleteBtn = document.querySelector(".list__deleteBtn");
+const form = document.querySelector(".app__form");
